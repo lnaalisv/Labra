4cbf1bbd6e7ce9d9bce4a9a4042e1a74599b9960
Lauri Naalisvaara <lauri.naalisvaara@aalto.fi>
2014-03-23 22:10:51 +0200

    faa

--- run_cs_tests.bat
+++ run_cs_tests.bat
@@ -3,8 +3,8 @@
 "cs/LabraCS/LabraCS/bin/Release/LabraCS.exe" test3 "data/book1.txt" almost 100 >> output/cs_test_3.txt
 "cs/LabraCS/LabraCS/bin/Release/LabraCS.exe" test4 "data/json.txt" 100 >> output/cs_test_4.txt
 "cs/LabraCS/LabraCS/bin/Release/LabraCS.exe" test5 "data/aalto_ericsson_visit" 1 >> output/cs_test_5.txt
-"cs/LabraCS/LabraCS/bin/Release/LabraCS.exe" test6 "data/aalto_ericsson_visit" csfileoutput 1 >> output/cs_test_6.txt
+"cs/LabraCS/LabraCS/bin/Release/LabraCS.exe" test6 "data/aalto_ericsson_visit" output2/csfileoutput 1 >> output/cs_test_6.txt
 "cs/LabraCS/LabraCS/bin/Release/LabraCS.exe" test7 100 100 1000 100 >> output/cs_test_7.txt
 "cs/LabraCS/LabraCS/bin/Release/LabraCS.exe" test8 500 100 1000 1000 >> output/cs_test_8.txt
-"cs/LabraCS/LabraCS/bin/Release/LabraCS.exe" test9 "data/adult_small.csv" 100 cs_output9.txt >> output/cs_test_9.txt
-"cs/LabraCS/LabraCS/bin/Release/LabraCS.exe" test9 "data/adult.csv" 10 cs_output10.txt >> output/cs_test_10.txt
+"cs/LabraCS/LabraCS/bin/Release/LabraCS.exe" test9 "data/adult_small.csv" 100 output2/cs_output9.txt >> output/cs_test_9.txt
+"cs/LabraCS/LabraCS/bin/Release/LabraCS.exe" test9 "data/adult.csv" 10 output2/cs_output10.txt >> output/cs_test_10.txt
--- run_php_tests.bat
+++ run_php_tests.bat
@@ -3,8 +3,8 @@ php -f "php/test2.php" -- 100 100 1000 50 >> output/php_test_2.txt
 php -f "php/test3.php" -- "data/book1.txt" almost 100 >> output/php_test_3.txt
 php -f "php/test4.php" -- "data/json.txt" 100 >> output/php_test_4.txt
 php -f "php/test5.php" -- "data/aalto_ericsson_visit" 1 >> output/php_test_5.txt
-php -f "php/test6.php" -- "data/aalto_ericsson_visit" phpfileoutput 1 >> output/php_test_6.txt
+php -f "php/test6.php" -- "data/aalto_ericsson_visit" output2/phpfileoutput 1 >> output/php_test_6.txt
 php -f "php/test7.php" -- 100 100 1000 100 >> output/php_test_7.txt
 php -f "php/test8.php" -- 500 100 1000 1000 >> output/php_test_8.txt
-php -f "php/test9.php" -- "data/adult_small.csv" 100 php_output9.txt >> output/php_test_9.txt
-php -f "php/test9.php" -- "data/adult.csv" 10 php_output10.txt >> output/php_test_10.txt
+php -f "php/test9.php" -- "data/adult_small.csv" 100 output2/php_output9.txt >> output/php_test_9.txt
+php -f "php/test9.php" -- "data/adult.csv" 10 output2/php_output10.txt >> output/php_test_10.txt
