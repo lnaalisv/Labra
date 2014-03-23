9a2cf5381079be80c0ca478a9ab7c06638fe8504
Lauri Naalisvaara <lauri.naalisvaara@aalto.fi>
2014-03-23 20:55:32 +0200

    cs project

--- cs/LabraCS/LabraCS/Program.cs
+++ cs/LabraCS/LabraCS/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections;
 
 namespace LabraCS
 {
@@ -49,50 +53,329 @@ namespace LabraCS
             }
         }
 
+        public static long memory_get_usage()
+        {
+            Process currentProcess = System.Diagnostics.Process.GetCurrentProcess();
+            return currentProcess.WorkingSet64;
+        }
+
+        public static int[,] get_matrix(int width, int height, int min_var, int max_var,Random rand) {
+            int[,] matrix = new int[width, height];
+            for(int x = 0 ; x < width ; x++) {
+                for(int y = 0 ; y < height ; y++) {
+                 matrix[x,y] = rand.Next(min_var,max_var);
+                }
+            }
+            return matrix;
+        }
+	    public static int[,] matrixmult(int[,] m1, int[,] m2) {
+            int r = m1.GetLength(0);
+            int c = m2.GetLength(1);
+            int p = m2.GetLength(0);
+		    int[,] m3 = new int[r,c];
+		    for(int i = 0 ; i < r ; i++) {
+			    for(int j = 0 ; j < c ; j++) {
+				    m3[i,j] = 0;
+				    for(int k = 0 ; k < p ; k++) {
+					    m3[i,j] += m1[i,k]*m2[k,j];
+				    }
+			    }
+		    }
+		    return m3;
+	    }
+        
         public static void test1(string[] args)
         {
             if (args.Length != 5)
             {
                 Console.WriteLine("Invalid number of arguments for test1");
+                return;
             }
-            int test_times =;
-            int min_var = ;
-            int max_var = ;
+            int test_times = Convert.ToInt32(args[1]);
+            int min_var = Convert.ToInt32(args[2]);
+            int max_var = Convert.ToInt32(args[3]);
+            int test_size = Convert.ToInt32(args[4]);
+
+            Random rand = new Random();
+            int a = rand.Next(min_var,max_var);
+            int b = rand.Next(min_var,max_var);
+            int c = rand.Next(min_var,max_var);
+            int d = rand.Next(min_var,max_var);
+            int e = rand.Next(min_var,max_var);
+            int f = rand.Next(min_var,max_var);
+            int g = rand.Next(min_var,max_var);
+            int h = rand.Next(min_var,max_var);
+
+            long mem1 = memory_get_usage();
+            long mem2 = 0;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for(int i = 0 ; i < test_times ; i++) {
+                float result = 0;
+                for (int j = 0; j < test_size; j++)
+                {
+                    result += (a * b ^ 3) / c * (d - e ^ j) - f + (g / h);
+                }
+                mem2 = memory_get_usage();
+            }
+
+            sw.Stop();
+            double delta = sw.Elapsed.TotalMilliseconds * 1000;
+            long mem3 = memory_get_usage();
+            Console.WriteLine(delta + ";" + mem1 + ";" + mem2 + ";" + mem3);
         }
 
         public static void test2(string[] args)
         {
+            if (args.Length != 5)
+            {
+                Console.WriteLine("Invalid number of arguments for test2");
+                return;
+            }
+            int test_times = Convert.ToInt32(args[1]);
+            int min_var = Convert.ToInt32(args[2]);
+            int max_var = Convert.ToInt32(args[3]);
+            int matrix_size = Convert.ToInt32(args[4]);
 
+            Random rand = new Random();
+            //int a = rand.Next(min_var, max_var);
+
+            long mem1 = memory_get_usage();
+            long mem2 = 0;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < test_times; i++)
+            {
+                int[,] m1 = get_matrix(matrix_size, matrix_size, min_var, max_var, rand);
+                int[,] m2 = get_matrix(matrix_size, matrix_size, min_var, max_var, rand);
+                matrixmult(m1,m2);
+                mem2 = memory_get_usage();
+            }
+
+            sw.Stop();
+            double delta = sw.Elapsed.TotalMilliseconds * 1000;
+            long mem3 = memory_get_usage();
+            Console.WriteLine(delta + ";" + mem1 + ";" + mem2 + ";" + mem3);
         }
 
         public static void test3(string[] args)
         {
+            if (args.Length != 4)
+            {
+                Console.WriteLine("Invalid number of arguments for test3");
+                return;
+            }
 
+            String file = args[1];
+            String word = args[2];
+            int test_times = Convert.ToInt32(args[3]);
+
+            Random rand = new Random();
+            //int a = rand.Next(min_var, max_var);
+
+            long mem1 = memory_get_usage();
+            long mem2 = 0;
+
+            String[] lines = File.ReadAllLines(file);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < test_times; i++)
+            {
+                for (int k = 0; k < lines.Length; k++ )
+                {
+                    String line = lines[k];
+                    if (line.Contains(word))
+                    {
+                        //Console.WriteLine("Sana " + word + " esiintyy rivillä " + k);
+                    }
+                }
+                mem2 = memory_get_usage();
+            }
+
+            sw.Stop();
+            double delta = sw.Elapsed.TotalMilliseconds * 1000;
+            long mem3 = memory_get_usage();
+            Console.WriteLine(delta + ";" + mem1 + ";" + mem2 + ";" + mem3);
         }
 
         public static void test4(string[] args)
         {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Invalid number of arguments for test4");
+                return;
+            }
 
+            String file = args[1];
+            int test_times = Convert.ToInt32(args[2]);
+
+            long mem1 = memory_get_usage();
+            long mem2 = 0;
+
+            String lines = File.ReadAllText(file);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < test_times; i++)
+            {
+                object json = JsonConvert.DeserializeObject(lines);
+                mem2 = memory_get_usage();
+            }
+
+            sw.Stop();
+            double delta = sw.Elapsed.TotalMilliseconds * 1000;
+            long mem3 = memory_get_usage();
+            Console.WriteLine(delta + ";" + mem1 + ";" + mem2 + ";" + mem3);
         }
 
         public static void test5(string[] args)
         {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Invalid number of arguments for test5");
+                return;
+            }
 
+            String file = args[1];
+            int test_times = Convert.ToInt32(args[2]);
+
+            long mem1 = memory_get_usage();
+            long mem2 = 0;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < test_times; i++)
+            {
+                byte[] bytes = File.ReadAllBytes(file);
+                mem2 = memory_get_usage();
+            }
+
+            sw.Stop();
+            double delta = sw.Elapsed.TotalMilliseconds * 1000;
+            long mem3 = memory_get_usage();
+            Console.WriteLine(delta + ";" + mem1 + ";" + mem2 + ";" + mem3);
         }
 
         public static void test6(string[] args)
         {
+            if (args.Length != 4)
+            {
+                Console.WriteLine("Invalid number of arguments for test6");
+                return;
+            }
 
+            String file = args[1];
+            String output = args[2];
+            int test_times = Convert.ToInt32(args[3]);
+
+            long mem1 = memory_get_usage();
+            long mem2 = 0;
+
+            byte[] bytes = File.ReadAllBytes(file);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < test_times; i++)
+            {
+                File.WriteAllBytes(output, bytes);
+                mem2 = memory_get_usage();
+            }
+
+            sw.Stop();
+            double delta = sw.Elapsed.TotalMilliseconds * 1000;
+            long mem3 = memory_get_usage();
+            Console.WriteLine(delta + ";" + mem1 + ";" + mem2 + ";" + mem3);
         }
 
         public static void test7(string[] args)
         {
+            if (args.Length != 5)
+            {
+                Console.WriteLine("Invalid number of arguments for test7");
+                return;
+            }
 
+            int test_times = Convert.ToInt32(args[1]);
+            int min_var = Convert.ToInt32(args[2]);
+            int max_var = Convert.ToInt32(args[3]);
+            int array_size = Convert.ToInt32(args[4]);
+
+            Random rand = new Random();
+            long mem1 = memory_get_usage();
+            long mem2 = 0;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < test_times; i++)
+            {
+                ArrayList list = new ArrayList();
+                for (int x = 0; x < array_size; x++ )
+                {
+                    ArrayList yy = new ArrayList();
+                    for (int y = 0; y < array_size; y++ )
+                    {
+                        yy.Add(rand.Next(min_var,max_var));
+                    }
+                    list.Add(yy);
+                }
+                int search_for = rand.Next(min_var, max_var);
+                foreach(ArrayList listitem in list) {
+                    if (listitem.IndexOf(search_for) != -1)
+                    {
+                        //Console.WriteLine(search_for + " found ! ");
+                    }
+                }
+
+                mem2 = memory_get_usage();
+            }
+
+            sw.Stop();
+            double delta = sw.Elapsed.TotalMilliseconds * 1000;
+            long mem3 = memory_get_usage();
+            Console.WriteLine(delta + ";" + mem1 + ";" + mem2 + ";" + mem3);
         }
 
         public static void test8(string[] args)
         {
+            if (args.Length != 5)
+            {
+                Console.WriteLine("Invalid number of arguments for test7");
+                return;
+            }
 
+            int test_times = Convert.ToInt32(args[1]);
+            int min_var = Convert.ToInt32(args[2]);
+            int max_var = Convert.ToInt32(args[3]);
+            int list_size = Convert.ToInt32(args[4]);
+
+            Random rand = new Random();
+            long mem1 = memory_get_usage();
+            long mem2 = 0;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < test_times; i++)
+            {
+                LinkedList<int> list = new LinkedList<int>();
+                for (int x = 0; x < list_size; x++)
+                {
+                    list.AddLast(rand.Next(min_var, max_var));
+                }
+
+                int search_for = rand.Next(min_var, max_var);
+                if (list.Find(search_for) != null)
+                {
+
+                }
+
+                mem2 = memory_get_usage();
+            }
+
+            sw.Stop();
+            double delta = sw.Elapsed.TotalMilliseconds * 1000;
+            long mem3 = memory_get_usage();
+            Console.WriteLine(delta + ";" + mem1 + ";" + mem2 + ";" + mem3);
         }
 
         public static void test9(string[] args)
--- run_java_tests.bat
+++ run_java_tests.bat
@@ -4,7 +4,7 @@ java -cp "java/bin/Labra.jar" Labra test3 "data/book1.txt" almost 100 >> output/
 java -cp "java/bin/Labra.jar" Labra test4 "data/json.txt" 100 >> output/java_test_4.txt
 java -cp "java/bin/Labra.jar" Labra test5 "data/aalto_ericsson_visit" 1 >> output/java_test_5.txt
 java -cp "java/bin/Labra.jar" Labra test6 "data/aalto_ericsson_visit" javafileoutput 1 >> output/java_test_6.txt
-java -cp "java/bin/Labra.jar" Labra test7 "data/aalto_ericsson_visit" 100 100 1000 100 >> output/java_test_7.txt
-java -cp "java/bin/Labra.jar" Labra test8 "data/aalto_ericsson_visit" 500 100 1000 1000 >> output/java_test_8.txt
+java -cp "java/bin/Labra.jar" Labra test7 100 100 1000 100 >> output/java_test_7.txt
+java -cp "java/bin/Labra.jar" Labra test8 500 100 1000 1000 >> output/java_test_8.txt
 java -cp "java/bin/Labra.jar" Labra test9 "data/adult_small.csv" 100 java_output9.txt >> output/java_test_9.txt
 java -cp "java/bin/Labra.jar" Labra test9 "data/adult.csv" 10 java_output10.txt >> output/java_test_10.txt
