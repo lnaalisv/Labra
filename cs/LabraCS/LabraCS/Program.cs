9f3b44e7f8704eb4867a4094071c3c81ff16f74e
Lauri Naalisvaara <lauri.naalisvaara@aalto.fi>
2014-03-23 19:48:11 +0200

    aasd

---
+++ cs/LabraCS/LabraCS/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabraCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Invalid number of arguments");
+            }
+            switch (args[0])
+            {
+                case "test1":
+                    test1(args);
+                    break;
+                case "test2":
+                    test2(args);
+                    break;
+                case "test3":
+                    test3(args);
+                    break;
+                case "test4":
+                    test4(args);
+                    break;
+                case "test5":
+                    test5(args);
+                    break;
+                case "test6":
+                    test6(args);
+                    break;
+                case "test7":
+                    test7(args);
+                    break;
+                case "test8":
+                    test8(args);
+                    break;
+                case "test9":
+                    test9(args);
+                    break;
+                default:
+                    Console.WriteLine("Unknown test " +args[0]);
+                    break;
+            }
+        }
+
+        public static void test1(string[] args)
+        {
+            if (args.Length != 5)
+            {
+                Console.WriteLine("Invalid number of arguments for test1");
+            }
+            int test_times =;
+            int min_var = ;
+            int max_var = ;
+        }
+
+        public static void test2(string[] args)
+        {
+
+        }
+
+        public static void test3(string[] args)
+        {
+
+        }
+
+        public static void test4(string[] args)
+        {
+
+        }
+
+        public static void test5(string[] args)
+        {
+
+        }
+
+        public static void test6(string[] args)
+        {
+
+        }
+
+        public static void test7(string[] args)
+        {
+
+        }
+
+        public static void test8(string[] args)
+        {
+
+        }
+
+        public static void test9(string[] args)
+        {
+
+        }
+    }
+}
--- php/test1.php
+++ php/test1.php
@@ -13,31 +13,26 @@
 	
 	$mem1 = memory_get_usage(true);
 	$mem2 = 0;
+	
+	// init test
+	$a = rand($min_var,$max_var);
+	$b = rand($min_var,$max_var);
+	$c = rand($min_var,$max_var);
+	$d = rand($min_var,$max_var);
+	$e = rand($min_var,$max_var);
+	$f = rand($min_var,$max_var);
+	$g = rand($min_var,$max_var);
+	$h = rand($min_var,$max_var);
+	
+	$before = microtime(true);
 	for($i = 0 ; $i < $test_times ; $i++) {
-		// init test
-		$a = rand($min_var,$max_var);
-		$b = rand($min_var,$max_var);
-		$c = rand($min_var,$max_var);
-		$d = rand($min_var,$max_var);
-		$e = rand($min_var,$max_var);
-		$f = rand($min_var,$max_var);
-		$g = rand($min_var,$max_var);
-		$h = rand($min_var,$max_var);
-		
-		$before = microtime(true);
-		
 		test($test_size,$a,$b,$c,$d,$e,$f,$g,$h);
-		
-		$after = microtime(true);
-		
-		$delta_time = $after-$before;
-		$delta_time_c += $delta_time;
-
-		//echo "Test $i in $delta_time seconds where $mem1 and $mem2\n";
 	}
+	$after = microtime(true);
+	$delta_time = ($after-$before)*1000*1000;
 	$mem3 = memory_get_usage(true);
 	$delta_mem = $mem3-$mem1;
-	echo "$delta_time_c;$mem1;$mem2;$mem3;$delta_mem\n";
+	echo "$delta_time;$mem1;$mem2;$mem3;$delta_mem\n";
 	
 	function test($test_size,$a,$b,$c,$d,$e,$f,$g,$h) {
 		global $mem2;
